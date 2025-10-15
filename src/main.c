59702c2e1466701f1d87e8bce17456063cd04990
mitch115 <m395l680@ku.edu>
2025-10-15 00:23:43 -0500

    test code

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+
+LOG_MODULE_REGISTER(main);
+int main(void)
+{
+        printk("nRF9151 test running\n");
+        while (1) {
+
+        LOG_INF("1");
+        //printk("running\n");
+        k_msleep(1000);
+        LOG_INF("2");
+        k_msleep(1000);
+        LOG_INF("3");
+        k_msleep(1000);
+        LOG_INF("4");
+        k_msleep(1000);
+        LOG_INF("5");
+        }
+        return 0;
+}
